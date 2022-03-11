c60d4716cea0e351d5ad54cc79439a5f8fa271f2
dayjang <dianna.jang@gmail.com>
2022-03-11 08:56:56 -0500

    middleware 1

--- src/server.js
+++ src/server.js
@@ -14,7 +14,13 @@ const handleLogin = (req, res) => {
   return res.send("Login Here");
 };
 
-app.get("/", handleHome);
+const middleWare = (req, res, next) => {
+  console.log(`SB is going throu middle ware.. ${req.url}`);
+  // return res.send("MIDDEL WARE ? ");
+  next();
+};
+
+app.get("/", middleWare, handleHome);
 app.get("/login", handleLogin);
 
 app.listen(PORT, handleListening);
