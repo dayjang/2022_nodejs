7106a100e9ad19e1ac49dedf87b3a72bf3d31f08
dayjang <dianna.jang@gmail.com>
2022-03-10 22:10:30 -0500

    get request

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ import express from "express";
 
 const app = express();
 const PORT = 4000;
-const handleListening = () => console.log(`server is listing... port: ${PORT}`);
+const handleListening = () =>
+  console.log(`server is listening... port: ${PORT}`);
+const handleHome = () => console.log("a user is trying to go to HOME 🏠");
+
+app.get("/", handleHome);
 app.listen(PORT, handleListening);
