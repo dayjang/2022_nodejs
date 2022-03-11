16334e5243d16ea689fe812a320a79e73aee9088
dayjang <dianna.jang@gmail.com>
2022-03-11 12:30:28 -0500

    morgan logger

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const PORT = 4000;
+const logger = morgan("combined");
 
 const handleListening = () =>
   console.log(`server is listening... port: ${PORT}`);
@@ -14,23 +16,7 @@ const handleLogin = (req, res) => {
   return res.send("Login Here");
 };
 
-const logger = (req, res, next) => {
-  console.log(`SB is going throu middle ware..${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddle = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    console.log("ALERT!");
-    return res.send("<h2>NOT ALLOWED</h2>");
-  }
-  console.log("allowed, you can continue......");
-  next();
-};
 app.use(logger);
-app.use(privateMiddle);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
-
 app.listen(PORT, handleListening);
